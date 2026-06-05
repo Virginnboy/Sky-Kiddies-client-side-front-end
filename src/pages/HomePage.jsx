7a6222b24ecba243bae6a153002f24240ade52a2
Virginnboy <virginboyudemy@gmail.com>
2026-06-05 17:26:20 +0100

    modified homepage

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,52 +1,52 @@
-import { useOutletContext } from "react-router-dom";
-import { useMutation } from "@tanstack/react-query";
-import { addToCart } from "../store/util"
-import { Truncate } from "../components/Truncating";
-import { formattedCurrency } from "../store/formattedCurrency";
-import { useNavigate } from "react-router-dom";
-import Loading from "../components/Loading";
-import { Link } from "react-router-dom";
-import "../pages/HomePage.css";
-import toast from "react-hot-toast";
+// import { useOutletContext } from "react-router-dom";
+// import { useMutation } from "@tanstack/react-query";
+// import { addToCart } from "../store/util"
+// import { Truncate } from "../components/Truncating";
+// import { formattedCurrency } from "../store/formattedCurrency";
+// import { useNavigate } from "react-router-dom";
+// import Loading from "../components/Loading";
+// import { Link } from "react-router-dom";
+// import "../pages/HomePage.css";
+// import toast from "react-hot-toast";
 
 
 export default function HomePage() {
-  const navigate = useNavigate();
-  const  { products, isLoading, isQueryError, queryError} = useOutletContext();
+//   const navigate = useNavigate();
+//   const  { products, isLoading, isQueryError, queryError} = useOutletContext();
 
-// ADD TO CART
-  const { mutate, isPending } = useMutation({
-    mutationFn: addToCart,
-    onSuccess: (res)=> {
-      console.log(res.data?.message)
-    },
-    onError: (err)=> {
-      console.log(err)
-      if (err.response?.status === 401) {
-        navigate("/login")
-      }
-      toast.error(err.response?.data?.message || err.response?.message)
-    }
-  })
+// // ADD TO CART
+//   const { mutate, isPending } = useMutation({
+//     mutationFn: addToCart,
+//     onSuccess: (res)=> {
+//       console.log(res.data?.message)
+//     },
+//     onError: (err)=> {
+//       console.log(err)
+//       if (err.response?.status === 401) {
+//         navigate("/login")
+//       }
+//       toast.error(err.response?.data?.message || err.response?.message)
+//     }
+//   })
 
 
-  if (isLoading) {
-    return <Loading/>
-  }
+//   if (isLoading) {
+//     return <Loading/>
+//   }
 
-  if (isQueryError) {
-    return <p>Error: {queryError.data?.message}</p>
-  };  
+//   if (isQueryError) {
+//     return <p>Error: {queryError.data?.message}</p>
+//   };  
 
-  const handleAddToCart = (data)=> {
-    mutate(data)
-  }
+//   const handleAddToCart = (data)=> {
+//     mutate(data)
+//   }
 
 
   return (
     <>
       <div className="products-container">
-        <ul>
+        {/* <ul>
           {products?.map((product)=> <Link key={product._id} className="product-list" to={`/product-details/${product._id}`}>
             <div className="image-container">
               <img src={product.images[0]} alt={product.title}/>
@@ -72,7 +72,7 @@ export default function HomePage() {
                 product._id)}} >{isPending ? "Adding..." : "Add to Cart"}</button>
             </div>
           </Link>)}
-        </ul>
+        </ul> */}
       </div>
     </>
   )
