1a37c8cd392d209364bfb6d381397599213ef6e7
Virginnboy <virginboyudemy@gmail.com>
2026-06-26 00:12:16 +0100

    adding base url direct into the axios

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,6 +1,6 @@
 import axios from "axios"
 
-const API_URL = import.meta.env.VITE_API_URL; 
+const API_URL = "https://sky-kiddies-back-end.onrender.com"
 console.log("API_URL:", API_URL);
 
 export const api = axios.create({
