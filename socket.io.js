72d0338da2ce87f1990f21aa7ac1f57adf090de7
Virginnboy <virginboyudemy@gmail.com>
2026-08-15 20:19:43 +0100

    changed socket authentication by using cookie rather than localstorage token

--- socket.io.js
+++ socket.io.js
@@ -1,7 +1,8 @@
 import { io } from "socket.io-client";
 
-const token = localStorage.getItem("userToken")
-
 export const socket = io("https://sky-kiddies-back-end.onrender.com", {
-  auth: {token}
+  withCredentials: true,
+  auth: {
+    type: "user"
+  }
 }); 
